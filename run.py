61ad31f1fdd59294f61a2d8cce6db5cc9b626f16
bdolan55 <brian.e.dolan88@gmail.com>
2023-03-02 14:33:29 +0000

    adding required functions for the operation of the gamne

--- run.py
+++ run.py
@@ -9,3 +9,16 @@ char_to_num = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
 
 def board():
     pass
+
+
+def create_ships():
+    pass
+
+
+def get_ship_location():
+    pass
+
+
+create_ships()
+TURNS = 10
+# while turns > 0:
