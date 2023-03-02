2f424ccc7844a78013c4a2f0b19395826d4a58d5
bdolan55 <brian.e.dolan88@gmail.com>
2023-03-02 14:58:09 +0000

    chreate ships function added and fixing of syntax errors and whitespace

--- run.py
+++ run.py
@@ -1,18 +1,33 @@
+from random import randint
 
 
-HIDDEN_BOARD = [{" "} * 8 for x in range(8)]
-GUESS_BOARD = [{" "} * 8 for x in range(8)]
+HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
+GUESS_BOARD = [[" "] * 8 for x in range(8)]
 
 
-char_to_num = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
+char_to_num = {
+    "A": 0, "B": 1,
+    "C": 2, "D": 3,
+    "E": 4, "F": 5,
+    "G": 6, "H": 7
+    }
 
 
-def board():
-    pass
+def print_board(board):
+    print("    A B C D E F G H")
+    print("    ---------------")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
 
 
 def create_ships():
-    pass
+    for ship in range(5):
+        ship_row, ship_column = randint(0, 7), randint(0, 7)
+        while board[ship_row][ship_column] == "X":
+            ship_row, ship_column = randint(0, 7), randint(0, 7)
+        board[ship_row][ship_column] = "X"
 
 
 def get_ship_location():
